f0907a672401f9be75be2f95569fb7d442b65bac
Gabriela D'Avila <gabidavila@gmail.com>
2014-08-30 16:28:18 +0200

    Added Newsletter form

--- resources/views/hello.php
+++ resources/views/hello.php
@@ -19,7 +19,9 @@
         <div class="col-md-12">
             <div class="well pull-right">
                 <p><a href="http://www.twitter.com/gabidavila"><i class="fa fa-twitter fa-lg"></i> Twitter</a></p>
-                <p><a href="http://www.github.com/wediversifi/wediversifi.com"><i class="fa fa-github fa-lg"></i> Github</a></p>
+
+                <p><a href="http://www.github.com/wediversifi/wediversifi.com"><i class="fa fa-github fa-lg"></i> Github</a>
+                </p>
             </div>
             <h1>WeDiversifi</h1>
             <article class="markdown-body entry-content" itemprop="mainContentOfPage">
@@ -85,6 +87,87 @@
             </p>
         </div>
     </div>
+    <hr>
+    <div class="row">
+        <div class="col-md-6">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Subscribe</h3>
+                </div>
+                <div class="panel-body">
+                    <!-- Begin MailChimp Signup Form -->
+                    <link href="//cdn-images.mailchimp.com/embedcode/classic-081711.css" rel="stylesheet"
+                          type="text/css">
+                    <style type="text/css">
+                        #mc_embed_signup {
+                            background: #fff;
+                            clear: left;
+                            font: 14px Helvetica, Arial, sans-serif;
+                        }
+
+                        /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+                           We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+                    </style>
+                    <div id="mc_embed_signup">
+                        <form
+                            action="//wediversifi.us9.list-manage.com/subscribe/post?u=212b0bfecb6da0593d26e97ad&amp;id=a55d389a66"
+                            method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form"
+                            class="validate"
+                            target="_blank" novalidate>
+                            <h2>Subscribe to our mailing list</h2>
+
+                            <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+                            <div class="mc-field-group">
+                                <label for="mce-EMAIL">Email Address <span class="asterisk">*</span>
+                                </label>
+                                <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+                            </div>
+                            <div class="mc-field-group">
+                                <label for="mce-FNAME">First Name </label>
+                                <input type="text" value="" name="FNAME" class="" id="mce-FNAME">
+                            </div>
+                            <div class="mc-field-group">
+                                <label for="mce-LNAME">Last Name </label>
+                                <input type="text" value="" name="LNAME" class="" id="mce-LNAME">
+                            </div>
+                            <div class="mc-field-group input-group">
+                                <strong>I want to <span class="asterisk">*</span>
+                                </strong>
+                                <ul>
+                                    <li><input type="radio" value="Contribute &amp; Stay Informed" name="IWANTTO"
+                                               id="mce-IWANTTO-0"><label for="mce-IWANTTO-0">Contribute &amp; Stay
+                                            Informed</label>
+                                    </li>
+                                    <li><input type="radio" value="Only Stay Informed" name="IWANTTO"
+                                               id="mce-IWANTTO-1"><label
+                                            for="mce-IWANTTO-1"> Only Stay Informed</label></li>
+                                </ul>
+                            </div>
+                            <div id="mce-responses" class="clear">
+                                <div class="response" id="mce-error-response" style="display:none"></div>
+                                <div class="response" id="mce-success-response" style="display:none"></div>
+                            </div>
+                            <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                            <div style="position: absolute; left: -5000px;"><input type="text"
+                                                                                   name="b_212b0bfecb6da0593d26e97ad_a55d389a66"
+                                                                                   tabindex="-1" value=""></div>
+                            <div class="clear"><input type="submit" value="Subscribe" name="subscribe"
+                                                      id="mc-embedded-subscribe"
+                                                      class="button"></div>
+                        </form>
+                    </div>
+
+                    <!--End mc_embed_signup-->
+                </div>
+
+            </div>
+        </div>
+        <div class="col-md-2">
+            <h3>Doubts?</h3>
+            <p>Please contact-us: <a href="http://twitter.com/gabidavila">@gabidavila</a> or <a href="http://twitter.com/kayladnls">@kayladnls</a></p>
+        </div>
+    </div>
+
 </div>
 <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 <!-- Latest compiled and minified JavaScript -->
